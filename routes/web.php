944e0e6fbe0af13e268aa3157c28694a7fea7f9f
kkokwara2014 <kkokwara2014@gmail.com>
2019-08-08 13:34:49 +0100

    Including the bootstrap template
    Add the bootstrap template and templating the pages.

--- routes/web.php
+++ routes/web.php
@@ -12,8 +12,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Route::get('/','PagesController@index')->name('index');
