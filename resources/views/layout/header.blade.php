6bb7bea7b099d619b31d64c753e82dfaab24b3e4
kkokwara2014 <kkokwara2014@gmail.com>
2019-08-10 20:20:15 +0100

    --- Auto-Git Commit ---

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -1,33 +1,38 @@
 <!doctype html>
 <html lang="en">
-    <head>
-        <!-- Required meta tags -->
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <link rel="icon" href="img/favicon.png" type="image/png">
-        <title>AJSS</title>
-        <!-- Bootstrap CSS -->
-        <link rel="stylesheet" href="{{asset('bootstrap_assets/css/bootstrap.css')}}">
-        <link rel="stylesheet" href="{{asset('bootstrap_assets/vendors/linericon/style.css')}}">
-        <link rel="stylesheet" href="{{asset('bootstrap_assets/css/font-awesome.min.css')}}">
-        <link rel="stylesheet" href="{{asset('bootstrap_assets/vendors/owl-carousel/owl.carousel.min.css')}}">
-        <link rel="stylesheet" href="{{asset('bootstrap_assets/vendors/lightbox/simpleLightbox.css')}}">
-        <link rel="stylesheet" href="{{asset('bootstrap_assets/vendors/nice-select/css/nice-select.css')}}">
-        <link rel="stylesheet" href="{{asset('bootstrap_assets/vendors/animate-css/animate.css')}}">
-        <!-- main css -->
-        <link rel="stylesheet" href="{{asset('bootstrap_assets/css/style.css')}}">
-        <link rel="stylesheet" href="{{asset('bootstrap_assets/css/responsive.css')}}">
-    </head>
-    <body>
-        
-        <!--================Header Menu Area =================-->
-        <header class="header_area">
-            <div class="main_menu">
-            	<nav class="navbar navbar-expand-lg navbar-light">
+
+	<head>
+		<!-- Required meta tags -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<link rel="icon" href="img/favicon.png" type="image/png">
+		<title>AJSS</title>
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="{{asset('bootstrap_assets/css/bootstrap.css')}}">
+		<link rel="stylesheet" href="{{asset('bootstrap_assets/vendors/linericon/style.css')}}">
+		<link rel="stylesheet" href="{{asset('bootstrap_assets/css/font-awesome.min.css')}}">
+		<link rel="stylesheet" href="{{asset('bootstrap_assets/vendors/owl-carousel/owl.carousel.min.css')}}">
+		<link rel="stylesheet" href="{{asset('bootstrap_assets/vendors/lightbox/simpleLightbox.css')}}">
+		<link rel="stylesheet" href="{{asset('bootstrap_assets/vendors/nice-select/css/nice-select.css')}}">
+		<link rel="stylesheet" href="{{asset('bootstrap_assets/vendors/animate-css/animate.css')}}">
+		<!-- main css -->
+		<link rel="stylesheet" href="{{asset('bootstrap_assets/css/style.css')}}">
+		<link rel="stylesheet" href="{{asset('bootstrap_assets/css/responsive.css')}}">
+	</head>
+
+	<body>
+
+		<!--================Header Menu Area =================-->
+		<header class="header_area">
+			<div class="main_menu">
+				<nav class="navbar navbar-expand-lg navbar-light">
 					<div class="container box_1620">
 						<!-- Brand and toggle get grouped for better mobile display -->
-						<a class="navbar-brand logo_h" href="index.html"><img src="{{asset('bootstrap_assets/img/logo.png')}}" alt=""></a>
-						<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+						<a class="navbar-brand logo_h" href="index.html"><img
+								src="{{asset('bootstrap_assets/img/logo.png')}}" alt=""></a>
+						<button class="navbar-toggler" type="button" data-toggle="collapse"
+							data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+							aria-expanded="false" aria-label="Toggle navigation">
 							<span class="icon-bar"></span>
 							<span class="icon-bar"></span>
 							<span class="icon-bar"></span>
@@ -35,29 +40,56 @@
 						<!-- Collect the nav links, forms, and other content for toggling -->
 						<div class="collapse navbar-collapse offset" id="navbarSupportedContent">
 							<ul class="nav navbar-nav menu_nav ml-auto">
-								<li class="nav-item active"><a class="nav-link" href="index.html">Home</a></li> 
-								<li class="nav-item"><a class="nav-link" href="about-us.html">About</a></li> 
-								
+								<li class="nav-item active"><a class="nav-link" href="index.html">Home</a></li>
+								<li class="nav-item"><a class="nav-link" href="about-us.html">About</a></li>
+
 								<li class="nav-item submenu dropdown">
-									<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Pages</a>
+									<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button"
+										aria-haspopup="true" aria-expanded="false">Menu</a>
 									<ul class="dropdown-menu">
-										<li class="nav-item"><a class="nav-link" href="schedule.html">Schedule</a>
-										<li class="nav-item"><a class="nav-link" href="venue.html">Venue</a>
-										<li class="nav-item"><a class="nav-link" href="price.html">Pricing</a> 
-										<li class="nav-item"><a class="nav-link" href="elements.html">Elements</a></li>
+										<li class="nav-item"><a class="nav-link" href="#">How to publish Paper</a></li>
+										<li class="nav-item"><a class="nav-link" href="#">Mode of Payment</a></li>
+										<li class="nav-item"><a class="nav-link" href="#">Query Paper Status</a></li>
 									</ul>
-								</li> 
-								
+								</li>
+								<li class="nav-item submenu dropdown">
+									<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button"
+										aria-haspopup="true" aria-expanded="false">Publication</a>
+									<ul class="dropdown-menu">
+										<li class="nav-item"><a class="nav-link" href="#">Charges</a></li>
+										<li class="nav-item"><a class="nav-link" href="#">Steps</a></li>
+										<li class="nav-item"><a class="nav-link" href="#">Ethics</a></li>
+										<li class="nav-item"><a class="nav-link" href="#">Review Process</a></li>
+									</ul>
+								</li>
+								<li class="nav-item submenu dropdown">
+									<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button"
+										aria-haspopup="true" aria-expanded="false">Downloads</a>
+									<ul class="dropdown-menu">
+										<li class="nav-item"><a class="nav-link" href="#">Copyright Form</a></li>
+										<li class="nav-item"><a class="nav-link" href="#">Paper Template</a></li>
+									</ul>
+								</li>
+
+								<li class="nav-item submenu dropdown">
+									<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button"
+										aria-haspopup="true" aria-expanded="false">Special Events</a>
+									<ul class="dropdown-menu">
+										<li class="nav-item"><a class="nav-link" href="#">Call for Conference</a></li>
+										<li class="nav-item"><a class="nav-link" href="#">Conference to Come</a></li>
+									</ul>
+								</li>
 								<li class="nav-item"><a class="nav-link" href="contact.html">Contact</a></li>
+
 							</ul>
 							<ul class="nav navbar-nav navbar-right">
-								<li class="nav-item"><a href="#" class="tickets_btn">Submit Paper Online</a></li>
-								<li class="nav-item"><a href="#" class="nav-link">ISSN : </a></li>
-								<li class="nav-item"><a href="#" class="nav-link">Email : </a></li>
+								<li class="nav-item"><a href="#" class="tickets_btn">Submit Paper</a></li>
+								{{-- <li class="nav-item"><small class="nav-link">ISSN: </small></li>
+								<li class="nav-item"><small class="nav-link">Email:</small></li> --}}
 							</ul>
-						</div> 
+						</div>
 					</div>
-            	</nav>
-            </div>
-        </header>
-        <!--================Header Menu Area =================-->
+				</nav>
+			</div>
+		</header>
+		<!--================Header Menu Area =================-->
