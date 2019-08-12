3d00673833da9956d1522855bc266c3efaa4aae0
kkokwara2014 <kkokwara2014@gmail.com>
2019-08-12 20:01:02 +0100

    --- Auto-Git Commit ---

--- resources/views/admin/editor/create.blade.php
+++ resources/views/admin/editor/create.blade.php
@@ -6,52 +6,80 @@
 <section class="content">
     <div class="row">
         <!-- Left col -->
-        <section class="col-lg-7 connectedSortable">
-
+        <section class="col-lg-9 connectedSortable">
+            <div><a href="#" class="btn btn-success btn-sm"><span class="fa fa-eye"></span> All Editors</a></div><p></p>
             <div class="row">
-                <div class="col-md-12">
+                <div class="col-md-12 col-lg-12">
                     <div class="panel">
                         <div class="panel-body">
                             <form role="form">
-                                <div class="form-group">
-                                    <label>Title</label>
-                                    <select class="form-control select2" style="width: 100%;">
-                                        <option selected="disabled">Select Title</option>
-                                        <option>Engr.</option>
-                                        <option >Dr.</option>
-                                        <option>Prof.</option>
-                                    </select>
+                                <div class="row">
+                                    <div class="col-sm-6 col-lg-6">
+                                        <div class="form-group">
+                                            <label>Title</label>
+                                            <select name="title" class="form-control select2" style="width: 100%;">
+                                                <option selected="disabled">Select Title</option>
+                                                <option>Engr.</option>
+                                                <option>Dr.</option>
+                                                <option>Prof.</option>
+                                            </select>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="surname">Surname</label>
+                                            <input type="text" class="form-control" id="surname" name="surname"
+                                                placeholder="Enter Surname">
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="firstname">First Name</label>
+                                            <input type="text" class="form-control" id="firstname" name="firstname"
+                                                placeholder="Enter First Name">
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="othername">Other Name</label>
+                                            <input type="text" class="form-control" id="othername" name="othername"
+                                                placeholder="Enter Other Name">
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="organization">Organization</label>
+                                            <input type="text" class="form-control" id="organization"
+                                                name="organization"
+                                                placeholder="Enter Organization e.g. Akanu Ibiam Federal Poly. Unwana">
+                                        </div>
+                                    </div>
+
+                                    <div class="col-sm-6 col-lg-6">
+                                            <div class="form-group">
+                                                <label for="department">Department</label>
+                                                <input type="text" class="form-control" id="Department" name="department"
+                                                    placeholder="Enter Department e.g. Computer Science">
+                                            </div>
+                                            <div class="form-group">
+                                                <label>Country</label>
+                                                <select name="country_id" class="form-control select2" style="width: 100%;">
+                                                    <option selected="disabled">Select Country</option>
+                                                    <option>Engr.</option>
+                                                    <option>Dr.</option>
+                                                    <option>Prof.</option>
+                                                </select>
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="phone">Phone</label>
+                                                <input type="tel" class="form-control" id="phone" name="phone"
+                                                    placeholder="Enter phone e.g. 08038883919" maxlength="11">
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="email">Email Address</label>
+                                                <input type="email" class="form-control" id="email" name="email"
+                                                    placeholder="Enter Email Address">
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="editor_image">Editor Image</label>
+                                                <input type="file" id="editor_image" name="editor_image">
+                                            </div>
+                                        </div>
                                 </div>
-                                <div class="form-group">
-                                    <label for="surname">Surname</label>
-                                    <input type="email" class="form-control" id="surname" name="surname"
-                                        placeholder="Enter Surname">
-                                </div>
-                                <div class="form-group">
-                                    <label for="firstname">First Name</label>
-                                    <input type="email" class="form-control" id="firstname" name="firstname"
-                                        placeholder="Enter First Name">
-                                </div>
-                                <div class="form-group">
-                                    <label for="othername">Other Name</label>
-                                    <input type="email" class="form-control" id="othername" name="othername"
-                                        placeholder="Enter Other Name">
-                                </div>
-                                <div class="form-group">
-                                    <label for="organization">Organization</label>
-                                    <input type="email" class="form-control" id="organization" name="organization"
-                                        placeholder="Enter Organization e.g. Akanu Ibiam Federal Poly. Unwana">
-                                </div>
-                                <div class="form-group">
-                                    <label for="exampleInputPassword1">Password</label>
-                                    <input type="password" class="form-control" id="exampleInputPassword1"
-                                        placeholder="Password">
-                                </div>
-                                <div class="form-group">
-                                    <label for="editor_image">File input</label>
-                                    <input type="file" id="editor_image" name="editor_image">
-                                </div>
-                                <button type="submit" class="btn btn-primary">Submit</button>
+
+                                <button type="submit" class="btn btn-primary">Add Editor</button>
 
                             </form>
                         </div>
