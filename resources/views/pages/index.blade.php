98b263ac0492c4a8dee94ca61397cb18afa97a99
kkokwara2014 <kkokwara2014@gmail.com>
2019-08-13 23:52:15 +0100

    Update index.blade.php

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -55,7 +55,7 @@
 
             <div class="col-lg-9 col-sm-9" style="text-align: justify">
                 <div class="welcome_text">
-                   
+
                     <p>inappropriate behavior is often laughed off as “boys will be boys,” women face higher conduct
                         standards especially in the workplace. That’s why it’s crucial that, as women, our behavior on
                         the job is beyond reproach. inappropriate behavior is often laughed.</p>
@@ -75,13 +75,31 @@
 <!--================Editorial Board Area =================-->
 <section class="team_area p_120">
     <div class="container">
-        <div class="main_title" style="text-align: left">
-            <h2>Our Editorial Board</h2>
-            <div style="text-align: left">If you are looking at blank cassettes on the web, you may be very confused at
-                the difference in price.
-                You may see some for as low as $.17 each.</div>
+        <div class="row">
+            <div class="col-lg-6">
+                <div class="main_title" style="text-align: left">
+                    <h2>Editorial Board</h2>
+                    <div style="text-align: justify">Our outstanding Editorial Board team are working round the clock to
+                        ensure that quality research work are published. Submitted papers are thoroughly reviewed before
+                        publication is made. To see details of Editorial Board,  
+                        <a href="{{ route('editors') }} " class="badge badge-info"> Read more...</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="main_title" style="text-align: left">
+                    <h2>Special Events</h2>
+                    <div style="text-align: justify">
+                        AJSSAIFPU ensure that other activities which promote sharing of quality 
+                        research work keep happening throughotu the year. You can visit our 
+                        <a href="{{ route('callforpaper') }}" class="btn btn-info btn-sm"> Call for Conference</a> page or 
+                        <a href="{{ route('futureconference') }}" class="btn btn-info btn-sm"> Future Conference</a> page <br>
+                        to get more information.
+                    </div>
+                </div>
+            </div>
         </div>
-        
+
     </div>
 </section>
 <!--================End Editorial Board Area =================-->
