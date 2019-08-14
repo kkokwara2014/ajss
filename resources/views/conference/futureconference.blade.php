b00a4314f56adf68b8a09869a795cc809a496119
kkokwara2014 <kkokwara2014@gmail.com>
2019-08-14 02:09:51 +0100

    --- Auto-Git Commit ---

--- resources/views/conference/futureconference.blade.php
+++ resources/views/conference/futureconference.blade.php
@@ -48,20 +48,122 @@
         <!--================Welcome Area =================-->
         <section class="welcome_area pad_btm">
             <div class="container">
-                    
+
                 <div class="welcome_inner row" style="margin-top: 50px;">
 
                     <div class="col-lg-9 col-sm-9">
                         <div class="welcome_text">
-                                <h3>Future Conference</h3>
+                            <h3>Future Conference</h3>
 
-                            <div class=""></div>
+                            <!--Accordion wrapper-->
+                            <div class="accordion md-accordion accordion-1" id="accordionEx23" role="tablist">
+                                <div class="card">
+                                    <div class="card-header blue lighten-3 z-depth-1" role="tab" id="heading96">
+                                        <h5 class="text-uppercase mb-0 py-1">
+                                            <a class="white-text font-weight-bold" data-toggle="collapse"
+                                                href="#collapse96" aria-expanded="true" aria-controls="collapse96">
+                                                1<sup>st</sup> INTERNATIONAL CONFERENCE AND EXHIBITION
+                                            </a>
+                                        </h5>
+                                    </div>
+                                    <div id="collapse96" class="collapse" role="tabpanel"
+                                        aria-labelledby="heading96" data-parent="#accordionEx23">
+                                        <div class="card-body">
+                                            <div class="row my-4">
+                                                <div class="col-md-12">
+                                                    <h4 class="font-weight-bold mb-3 black-text">
+                                                        THEME : INNOVATIONS IN SCIENCE AND TECHNOLOGY: A TOOL FOR ECONOMIC DIVERSIFICATION.
+                                                    </h4>
+                                                    <p style="text-align: justify">
+                                                        <div><span class="fa fa-clock-o fa-2x"></span> </div>
+                                                    </p>
+                                                    <p class="mb-0">Duis aute irure dolor in reprehenderit in voluptate
+                                                        velit esse cillum dolore eu
+                                                        fugiat nulla pariatur.</p>
+                                                </div>
+                                                
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                {{-- <div class="card">
+                                    <div class="card-header blue lighten-3 z-depth-1" role="tab" id="heading97">
+                                        <h5 class="text-uppercase mb-0 py-1">
+                                            <a class="collapsed font-weight-bold white-text" data-toggle="collapse"
+                                                href="#collapse97" aria-expanded="false" aria-controls="collapse97">
+                                                I am the second title of accordion
+                                            </a>
+                                        </h5>
+                                    </div>
+                                    <div id="collapse97" class="collapse" role="tabpanel" aria-labelledby="heading97"
+                                        data-parent="#accordionEx23">
+                                        <div class="card-body">
+                                            <div class="row my-4">
+                                                <div class="col-md-8">
+                                                    <h2 class="font-weight-bold mb-3 black-text">Hi! I am the second
+                                                        one.</h2>
+                                                    <p class="">Lorem ipsum dolor sit amet, consectetur adipisicing
+                                                        elit, sed do eiusmod tempor
+                                                        incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                                                        veniam, quis nostrud exercitation
+                                                        ullamco laboris</p>
+                                                    <p class="mb-0">Duis aute irure dolor in reprehenderit in voluptate
+                                                        velit esse cillum dolore eu
+                                                        fugiat nulla pariatur.</p>
+                                                </div>
+                                                <div class="col-md-4 mt-3 pt-2">
+                                                    <div class="view z-depth-1">
+                                                        <img src="https://mdbootstrap.com/img/Photos/Others/nature.jpeg"
+                                                            alt="" class="img-fluid">
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="card">
+                                    <div class="card-header blue lighten-3 z-depth-1" role="tab" id="heading98">
+                                        <h5 class="text-uppercase mb-0 py-1">
+                                            <a class="collapsed font-weight-bold white-text" data-toggle="collapse"
+                                                href="#collapse98" aria-expanded="false" aria-controls="collapse98">
+                                                I am the third title of accordion
+                                            </a>
+                                        </h5>
+                                    </div>
+                                    <div id="collapse98" class="collapse" role="tabpanel" aria-labelledby="heading98"
+                                        data-parent="#accordionEx23">
+                                        <div class="card-body">
+                                            <div class="row my-4">
+                                                <div class="col-md-8">
+                                                    <h2 class="font-weight-bold mb-3 black-text">Hi! I am the third one.
+                                                    </h2>
+                                                    <p class="">Lorem ipsum dolor sit amet, consectetur adipisicing
+                                                        elit, sed do eiusmod tempor
+                                                        incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                                                        veniam, quis nostrud exercitation
+                                                        ullamco laboris</p>
+                                                    <p class="mb-0">Duis aute irure dolor in reprehenderit in voluptate
+                                                        velit esse cillum dolore eu
+                                                        fugiat nulla pariatur.</p>
+                                                </div>
+                                                <div class="col-md-4 mt-3 pt-2">
+                                                    <div class="view z-depth-1">
+                                                        <img src="https://mdbootstrap.com/img/Photos/Others/nature.jpeg"
+                                                            alt="" class="img-fluid">
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div> --}}
+                            </div>
+                            <!--Accordion wrapper-->
 
                         </div>
                     </div>
                     <div class="col-lg-3 col-sm-3">
-                            @include('layout.rightsidebar')
-                        </div>
+                        @include('layout.rightsidebar')
+                    </div>
                 </div>
             </div>
         </section>
